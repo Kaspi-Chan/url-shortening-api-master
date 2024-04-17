c5f281afd92c639d3f2be8a3dc60512f7221e3f6
Kaspi-Chan <bozhilov53@gmail.com>
2024-04-17 13:15:53 +0300

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline color-red bg-slate-100">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
