0791ad86dcca106bc1e729c000ffc3a64960055c
Kaspi-Chan <bozhilov53@gmail.com>
2024-04-17 13:47:59 +0300

    Setup up custom style properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline color-red bg-slate-100">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline color-red bg-primary-cyan font-poppins ">Hello world!</h1>
+      <p class="text-base font-normal">This paragraph will be 18px and have a font weight of 500.</p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-cyan": "hsl(180, 66%, 49%)",
+        "primary-violet": "hsl(257, 27%, 26%)",
+        "secondary-red": "hsl(0, 87%, 67%)",
+        "neutral-gray": "hsl(0, 0%, 75%)",
+        "neutral-grayish-violet": "hsl(257, 7%, 63%)",
+        "neutral-dark-blue": "hsl(255, 11%, 22%)",
+        "neutral-dark-violet": "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        'poppins': ["Poppins", "sans-serif"],
+      },
+      fontSize: {
+        'base': '1.125rem',
+      },
+      fontWeight: {
+        'normal': '500',
+      }
+    },
   },
   plugins: [],
 };
