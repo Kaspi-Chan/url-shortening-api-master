b3593782b931c996b140fc4004214b13d1481fea
Martin Bozhilov <bozhilov53@gmail.com>
2024-04-20 12:08:08 +0300

    Save work on Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import Header from "./components/Navbar";
+import PrimaryButton from './components/PrimaryButton';
+import bgImg from './assets/images/illustration-working.svg';
 
 function App() {
   return (
-    <main className="container mx-auto">
+    <main className="container mx-auto xl:px-10 overflow-x-hidden">
       <Header />
-
+      <section className="flex flex-col-reverse md:flex-row items-center justify-between gap-20 mt-10 px-5 md:px-0 h-[800px] md:h-[450px]">
+        <div className="text-center md:text-left md:w-1/2 h-1/2 md:h-full">
+          <h1 className="text-4xl lg:text-6xl xl:text-7xl lg:leading-tight text-primary-violet font-bold mb-3">More than just shorter links</h1>
+          <p className="text-neutral-gray text-lg font-normal mb-6 xl:mr-12">Build your brand's recognition and get detailed insights on how your links are performing.</p>
+          <PrimaryButton size="roundedBig" text={'Get Started'}/>
+        </div>
+        <div className="absolute w-[120%] md:w-full top-[10%] -right-[25%] md:-right-[55%]">
+          <img src={bgImg} alt="Background image" className="object-fit"/>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const buttonStyles = {
   roundedSmall: "py-2 px-4 rounded-full",
-  roundedBig: "py-2 px-6 rounded-full",
+  roundedBig: "py-3 px-8 rounded-full",
   rectangularSmall: "py-2 px-4 rounded-md",
   rectangularBig: "py-2 px-6 rounded-md",
 };
 
-const PrimaryButton = ({ size, text, onClick }) => {
+const PrimaryButton = ({ size = 'roundedSmall', text, onClick }) => {
   return (
     <button onClick={onClick} className={`text-white bg-primary-cyan hover:bg-primary-cyan-light font-bold ${buttonStyles[size]}`}>
       {text}
