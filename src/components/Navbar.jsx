000c46b7308c401135f92243ee43f88c76685433
Martin Bozhilov <bozhilov53@gmail.com>
2024-04-26 22:46:02 +0300

    Fix weird logo bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="container mx-auto md:px-7 2xl:px-20 py-5 sm:py-10 px-7 flex items-center gap-12 justify-between relative">
-      <HeaderLogo style={{ color: "#34313D" }} />
+      <HeaderLogo style={{ color: "#34313D", overflow: 'initial' }} />
       <Nav />
     </header>
   );
